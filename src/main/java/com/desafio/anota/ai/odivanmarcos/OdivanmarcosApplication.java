0ba1d13e27dc6fa190cb2c9328cfbcc05fc29ff4
odivanmarco <odivan.marcos@dtidigital.com.br>
2024-01-19 10:13:35 -0300

    Creating project Java with SpringBoot and MongoDB

---
+++ src/main/java/com/desafio/anota/ai/odivanmarcos/OdivanmarcosApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.anota.ai.odivanmarcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdivanmarcosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdivanmarcosApplication.class, args);
+	}
+
+}
