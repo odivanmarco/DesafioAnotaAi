1b16acd3f5017234ca9de396ebd1aecdda84f658
odivanmarco <odivanmarcos2@gmail.com>
2024-01-22 19:56:28 -0300

    feat(Controllers): Creating controller for Product

---
+++ src/main/java/com/desafio/anota/ai/odivanmarcos/controllers/ProductController.java
@@ -0,0 +1,43 @@
+package com.desafio.anota.ai.odivanmarcos.controllers;
+
+import com.desafio.anota.ai.odivanmarcos.domain.products.Product;
+import com.desafio.anota.ai.odivanmarcos.domain.products.ProductDto;
+import com.desafio.anota.ai.odivanmarcos.services.interfaces.IProductService;
+import jakarta.websocket.server.PathParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+    private final IProductService service;
+    public ProductController(IProductService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> insert(@RequestBody ProductDto dto){
+        Product product = this.service.insert(dto);
+        return ResponseEntity.ok().body(product);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll(){
+        List<Product> products = this.service.getAll();
+        return ResponseEntity.ok().body(products);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathParam("id") String id, @RequestBody ProductDto dto){
+        Product product = this.service.update(id, dto);
+        return ResponseEntity.ok().body(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> delete(@PathParam("id") String id){
+        this.service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
